dc6af8f2d80096a0dec2b90c7269ff7e80553e64
ybin3797 <u3797@naver.com>
2018-05-25 01:06:43 +0900

    Tree & addNode to panel

--- src/Elements.java
+++ src/Elements.java
@@ -94,7 +94,7 @@ class Mindmap extends Elements{
 	void drawNodes() {
 		for(int i=0;i<Node.length;i++) {
 			System.out.println("cnt : " + i);
-			drawNodePanel.add(Node[i]);
+			//drawNodePanel.add(Node[i]);
 			System.out.println("컴포수?~~"+drawNodePanel.getComponentCount());
 		}
 		System.out.println("dygh");
@@ -173,4 +173,164 @@ class Text extends Elements {
 		
 		
 	}
+}
+
+class Data{
+	private Data child;		//자식
+	private Data sibling;	//형제
+	private Data parent;	//부모
+	private String value;	//실제값
+	
+	public Data(String value) {this.value=value; child=null; sibling=null; parent=null;}
+	
+	public void setChild(Data obj) {child=obj;}
+	public Data getChild() {return child;}
+	
+	public void setSibling(Data obj) {sibling=obj;}
+	public Data getSibling() {return sibling;}
+	
+	public void setParent(Data obj) {parent=obj;}
+	public Data getParent() {return parent;}
+	
+	public String toString() {return value;}
+}
+
+class Tree{
+	Data start=null, last=null, obj=null;
+	
+	
+	int i=0;
+	
+	JLabel Make2Label(String value) {
+		return new JLabel(value);
+	}
+	
+	
+	void MakeTree(String [] member) {
+
+		int k=1;
+		for(int i=0;i<member.length;i++) {
+			if(start==null&&member[0].charAt(0)!='\t') { //첫 성분이 루트 (\t으로 시작 안한다.)
+				start=new Data(member[0]);
+				last=start;
+			}
+			
+			
+			else {
+				obj=new Data(member[i]);
+				
+				int lastTab=last.toString().lastIndexOf('\t');
+				int nowTab=obj.toString().lastIndexOf('\t');
+				
+				//여기서 계층구조 분류. \t로 시작할 것이다...만약 \t로 시작하지 않는다면 새로운 트리가 생기는 것 , 현재는 고려 X
+			
+				if(lastTab==nowTab) { //새로 추가된 노드가 최근 노드와의 같은 계층
+					obj.setParent(last.getParent());
+					last.setSibling(obj);
+				} 	
+				
+				else if(nowTab-lastTab==1) { //자식노드 추가
+					obj.setParent(last);
+					last.setChild(obj);
+					
+				}
+				
+				else { //last가 마지막 자식, 새로 추가된 녀석은 ... last보다 높은 계층
+					while(true) {
+						last=last.getParent();
+						if(last.toString().lastIndexOf('\t')==obj.toString().lastIndexOf('\t')) {
+							last.setSibling(obj);
+							break;
+						}
+						
+					}
+					
+				}
+				k++;
+				last=obj;
+				System.out.println(k);
+			}
+			
+		}
+		
+	}
+	
+	void print() {
+		int H=0;
+		Data k=start;
+		System.out.println(H+" "+k.toString());
+		while(true) {
+			
+			if(k.getChild()!=null) {
+				k=k.getChild();
+				H++;
+				System.out.println(H+" "+k.toString());
+			}
+			
+			else if(k.getSibling()!=null) {
+				k=k.getSibling();
+				System.out.println(H+" "+k.toString());
+			}
+			
+			else {
+				if(k==last) {
+					break;
+				}
+				while(true) {
+					k=k.getParent();
+					H--;
+					if(k.getSibling()!=null) {
+						k=k.getSibling();
+						System.out.println(H+" "+k.toString());
+						break;
+					}
+				}
+			}
+			
+		}
+	}
+	
+	void AddLabel(JPanel Panel) {
+		
+		Data k=start;
+
+		Panel.add(Make2Label(k.toString()));
+		
+		while(true) {
+			
+			if(k.getChild()!=null) {
+				k=k.getChild();
+				Panel.add(Make2Label(k.toString()));
+			}
+			
+			else if(k.getSibling()!=null) {
+				k=k.getSibling();
+				Panel.add(Make2Label(k.toString()));
+			}
+			
+			else {
+				if(k==last) {
+					break;
+				}
+				while(true) {
+					k=k.getParent();
+					if(k.getSibling()!=null) {
+						k=k.getSibling();
+						Panel.add(Make2Label(k.toString()));
+						break;
+					}
+				}
+			}
+			
+		}
+	}
+	
+	void Default() {
+		start=null;
+		last=null;
+		obj=null;
+	}
+	
+	
+	
 }
--- src/Events.java
+++ src/Events.java
@@ -1,4 +1,5 @@
 
+
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
@@ -7,25 +8,22 @@ import javax.swing.JLabel;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Font;
-//import java.awt.FlowLayout;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import java.util.StringTokenizer;
 
 
-class ButtonListener implements ActionListener { //버튼 이벤트
-	private Font basicFont = new Font("DialogInput", Font.BOLD,20);
-	
+class ButtonListener implements ActionListener{ //버튼 이벤트
 	private Object O;
 	private Mindmap mindmapSection;
 	//private JLabel [] MindMapNode;
 	private StringBuffer buffer; 						//text editor 값		-이 버튼이 실행될 때마다 새로운 객체 생기는 것이 찜찜함...
 	private String [] valueArray=new String[6];			//attribute editor 값	-이 버튼이 실행될 때마다 새로운 객체 생기는 것이 찜찜함...
 	private String tmp=new String();
+	private Tree tree=new Tree(); //TREE추가
 	
-//	JLabel [] Node;
 	
 	
 	ButtonListener(Object O,Mindmap mindmapSection) {
@@ -39,15 +37,17 @@ class ButtonListener implements ActionListener { //버튼 이벤트
 		
 		if(Btn.getText().equals("적용")) { 		//Text Editor
 			int i=0; 							//while문 카운트
-//			mindmapSection.drawNodePanel.removeAll();
+			mindmapSection.drawNodePanel.removeAll();
+			
+			
 			
 			buffer=new StringBuffer(((JTextArea)O).getText());
 		    tmp=buffer.toString();
 			StringTokenizer st=new StringTokenizer(tmp,"\n");	//st에 문자열에서 개행+문자로 된 애들 기준으로 자르기.
 			String [] member=new String[st.countTokens()];
 			//MindMapNode=new JLabel[st.countTokens()];
-			mindmapSection.Node=new JLabel[st.countTokens()];
-			System.out.println(st.countTokens());
+//			mindmapSection.Node=new JLabel[st.countTokens()];
+			//System.out.println(st.countTokens());
 			
 //			mindmapSection.drawNodePanel = new JPanel();//추가
 //			mindmapSection.drawNodePanel.setLayout(new FlowLayout());
@@ -55,19 +55,29 @@ class ButtonListener implements ActionListener { //버튼 이벤트
 			
 			while(st.hasMoreTokens()) {							//member배열에 \n기준으로 값 저장. (\t유효)
 				member[i]=st.nextToken();
-				System.out.println(member[i]);
-				mindmapSection.Node[i]=new JLabel(member[i]);				//MindMapNode배열 저장 OK
-				mindmapSection.Node[i].setOpaque(true);
-				mindmapSection.Node[i].setFont(basicFont);
-				mindmapSection.Node[i].setBackground(Color.CYAN);
-				mindmapSection.drawNodePanel.add(mindmapSection.Node[i]);					//여기가 문제... 출력이 ..안된다..
+//				System.out.println(member[i]);
+//				mindmapSection.Node[i]=new JLabel(member[i]);				//MindMapNode배열 저장 OK
+//				mindmapSection.Node[i].setOpaque(true);
+//				mindmapSection.Node[i].setBackground(Color.CYAN);
+//				mindmapSection.drawNodePanel.add(mindmapSection.Node[i]);					//여기가 문제... 출력이 ..안된다..
 				i++;
 			}
+			
+			
+			tree.MakeTree(member); //트리만들기...
+			tree.print();
+			tree.AddLabel(mindmapSection.drawNodePanel);
+			mindmapSection.drawNodePanel.setVisible(false);
+			mindmapSection.drawNodePanel.setVisible(true);
+			
+			
+			tree.Default();
+			
 //			mindmapSection.mindMapPanel.add(mindmapSection.drawNodePanel, BorderLayout.CENTER); //추가
 //			System.out.println("총 "+mindmapSection.drawNodePanel.getComponentCount()+"개의 요소");
-			mindmapSection.drawNodes();
+		//	mindmapSection.drawNodes();
 //			mindmapSection.drawNodePanel.setBackground(Color.white);
-			mindmapSection.mindMapPanel.add(mindmapSection.drawNodePanel,BorderLayout.CENTER);
+//			mindmapSection.mindMapPanel.add(mindmapSection.drawNodePanel,BorderLayout.CENTER);
 //			mindmapSection.drawNodePanel.pack();
 			
 		}
@@ -79,6 +89,7 @@ class ButtonListener implements ActionListener { //버튼 이벤트
 				valueArray[i-1]=value[i-1].getText();
 				System.out.println(valueArray[i-1]); //0번째에는 현재 빈 공간. NAME부분.
 			}
+			
 			
 		}
 		
